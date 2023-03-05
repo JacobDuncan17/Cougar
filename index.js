6b81b4379f335faf2951fef8a3264a5d30aa768c
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-03-04 20:32:57 -0700

    removed cli.js and moved it into the index.js. created all of the inquirer.prompt

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-const CLI = require('.lib/cli.js');
-
-const cli = new CLI ();
-
-cli.run();
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+class logoGenerator {
+    constructor () {
+        this.characters = '';
+        this.color = '';
+        this.shape = '';
+        this.shapeColor = '';
+        this.promptUser();
+    }
+  async promptUser() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'characters',
+        message: 'Enter up to three characters',
+        validate: function (value) {
+          var valid = value.length <= 3;
+          return valid || 'Please enter no more than 3 characters';
+        },
+      },
+      {
+        type: 'input',
+        name: 'color',
+        message: 'Enter text color (Can be keyword or hexadecimal)',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: Object.keys(shapes),
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (Can be keyword or hexadecimal)',
+      },
+    ]);
+    this.characters = answers.characters;
+    this.color = answers.color;
+    this.shape = answers.shape;
+    this.shapeColor = answers.shapeColor;
+  }
+}
